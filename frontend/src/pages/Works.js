3f1c53f39dff06ca07dae1525fb141f2feabe80b
Felix Fernando <felixzhuang1@gmail.com>
2022-01-11 23:58:13 +0700

    works > motion styling + querying

--- frontend/src/pages/Works.js
+++ frontend/src/pages/Works.js
@@ -1,14 +1,83 @@
 import React, {useState, useEffect} from 'react'
 import Navbar from '../components/Navbar'
+import { useQuery, gql } from '@apollo/client'
 
 import '../style/works.css'
+import { Link } from 'react-router-dom'
 
-function Still() {
 
-}
+const MOTIONS = gql`
+    query getMotions {
+        motions {
+            data {
+                attributes {
+                    title, 
+                    year, 
+                    link,
+                    thumbnail {
+                        data {
+                            attributes {
+                                url
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+`
+
+
+
+// function Still() {
+//     return(
+//         <></>
+//     )
+// }
 
 function Motion() {
+    const URL = "http://localhost:1337"
+    const { loading, error, data } = useQuery(MOTIONS)
 
+    if (loading) return <p>Loading</p>
+    if (error) return <p>Error</p>
+
+    console.log(data)
+    const motion_list = data.motions.data
+    const rev_motion_list = []
+
+    for(let i =  motion_list.length - 1; i >= 0; i--) {
+        rev_motion_list.push(motion_list[i])
+    }
+    return(
+        <>
+            {rev_motion_list.map(motion => {
+                return (
+                    <a href={motion.attributes.link} target="_blank">
+                        <div className='work-gallery'>
+                            <div className='work-detail'>
+                                <h1>{motion.attributes.title}</h1>
+                            </div>
+                            <img src={URL + motion.attributes.thumbnail.data.attributes.url}></img>
+                        </div>
+                    </a>
+                )
+            })}
+        </>
+    )
+}
+
+function WorksContainer(props) {
+    if (props.view == "motion") {
+        return (
+            <Motion></Motion>
+        )
+    } else {
+        return (
+            <></>
+        )
+    }
 }
 
 
@@ -30,7 +99,7 @@ function Works() {
                     }}className={workView == "still" ? "current" : ""}>Still</h1>
                 </div>
                 <div className='works-content-parent'>
-                    
+                    <WorksContainer view={workView}></WorksContainer>
                 </div>
             </div>
         </>
